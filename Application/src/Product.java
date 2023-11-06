52ae10e118c2fbd23814d59a351552b5d082de01
Leninadeee <christopherxu27@yahoo.com>
2023-11-06 18:10:18 -0500

    Edited coding style

--- Application/src/Product.java
+++ Application/src/Product.java
@@ -1,76 +1,50 @@
-import java.io.Serializable;
-
-public class Product implements Serializable {
-    private static final long serialVersionUID = 106L;
-    private Store store;
-    private String productName;
-    private String productDescription;
-    private int quantity;
+public class Product {
+    private String name;
+    private String description;
+    private int stock;
     private double price;
 
-    public Product(String productName , String productDescription , int quantity, int price , Store store) {
-        if (store == null || productName == null || productDescription == null ) {
-            throw new NullPointerException();
-        }
-        if (quantity < 0 || price < 0) {
-            throw new IllegalArgumentException();
-        }
-        this.productName = productName;
-        this.productDescription = productDescription;
-        this.quantity = quantity;
-        this.price = price;
-        this.store = store;
-    }
-
-    public void setProductName(String productName) {
-        if (productName == null) {
-            throw new NullPointerException();
-        }
-        this.productName = productName;
-    }
-
-    public void setProductDescription(String productDescription) {
-        if (productDescription == null) {
-            throw new NullPointerException();
-        }
-        this.productDescription = productDescription;
-    }
-
-    public void setQuantity(int quantity) {
-        if (quantity < 0 ) {
-            throw new IllegalArgumentException();
-        }
-        this.quantity = quantity;
-    }
-
-    public void setPrice(double price) {
-        if (price < 0) {
-            throw new IllegalArgumentException();
-        }
+    public Product(String name, String description, int stock, double price) {
+        this.name = name;
+        this.description = description;
+        this.stock = stock;
         this.price = price;
     }
-    public Store getStore() {
-        return store;
+
+    public String getName() {
+        return name;
     }
 
-    public String getProductName() {
-        return productName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public String getProductDescription() {
-        return productDescription;
+    public String getDescription() {
+        return description;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public String toString() {
-        return String.format("Product<Name: %s, Description: %s, Price: %.2f, Stock: %d, Store: %s>" , productName, productDescription, price, quantity, store.getName());
+    public void setPrice(double price) {
+        this.price = price;
     }
 
+    public String toString() {
+        return String.format("Name: %s\nDescription: %s\nStock: %d\nPrice: %.2f\n",
+                this.name, this.description, this.stock, this.price);
+    }
 }
---
+++ Application/src/Seller.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Seller extends User {
+    private ArrayList<Store> stores;
+
+    public Seller(String name, String password) {
+        super(name, password);
+    }
+
+    public void createStore(String storeName) {
+        stores.add(new Store(storeName));
+    }
+
+    public void removeStore(String storeName) {
+        for (int i = 0; i < stores.size(); i++) {
+            if (stores.get(i).getStoreName().equals(storeName)) {
+                stores.remove(i);
+                return;
+            }
+        }
+    }
+
+}
