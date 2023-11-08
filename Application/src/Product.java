240639229370c147ab889b071909c1408230fcca
Alex <143560174+Abenson0228@users.noreply.github.com>
2023-11-08 15:38:10 -0500

    Added comments for improved readability

--- Application/src/Product.java
+++ Application/src/Product.java
@@ -11,12 +11,13 @@
  *
  */
 public class Product {
-    private Store store;
-    private String productName;
-    private String productDescription;
-    private int stock;
-    private double price;
+    private Store store; //the store a product is associated with
+    private String productName; //the name of the product
+    private String productDescription; //the description of the product
+    private int stock; //the number of items in stock
+    private double price; //the price of the item
 
+     //basic constructor for Product class
     public Product(Store store, String productName, String productDescription, int stock, double price) {
         if (store == null || productName == null || productDescription == null) {
             throw new NullPointerException();
@@ -30,55 +31,55 @@ public class Product {
         this.stock = stock;
         this.price = price;
     }
-
+//sets Product name to given name, throws null pointer exception if null
     public void setProductName(String productName) {
         if (productName == null || productName.isEmpty()) {
             throw new NullPointerException();
         }
         this.productName = productName;
     }
-
+//sets the Product description, throws null pointer exception if null
     public void setProductDescription(String productDescription) {
         if (productDescription == null) {
             throw new NullPointerException();
         }
         this.productDescription = productDescription;
     }
-
+//set the stock to the given stock, throws Illegal argument exception if stock is less than zero
     public void setStock(int stock) {
         if (stock < 0 ) {
             throw new IllegalArgumentException();
         }
         this.stock = stock;
     }
-
+//sets the price to the given price, throws illegal argument exception if less than zero
     public void setPrice(double price) {
         if (price < 0) {
             throw new IllegalArgumentException();
         }
         this.price = price;
     }
-
+//returns the name of the store
     public Store getStore() {
         return store;
     }
-
+//returns the name of the product
     public String getProductName() {
         return productName;
     }
-
+//returns the description of the product
     public String getProductDescription() {
         return productDescription;
     }
-
+//returns the number of items in stock
     public int getStock() {
         return stock;
     }
-
+//returns the price
     public double getPrice() {
         return price;
     }
-
+//formats all of the information into a string and returns it
     public String toString() {
         return String.format("Product<Name: %s, Description: %s, Price: %.2f, Stock: %d, Store: %s>",
             this.getProductName(), this.getProductDescription(), this.getPrice(), this.getStock(),
