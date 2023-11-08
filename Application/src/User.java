df691f4fe392d382fc112448a7393d28453283d8
Alex <143560174+Abenson0228@users.noreply.github.com>
2023-11-08 15:45:37 -0500

    Added comments for improved readability

--- Application/src/User.java
+++ Application/src/User.java
@@ -4,20 +4,20 @@ public class User implements Serializable {
     private static final long serialVersionUID = 106L;
     private final String name;
     private final String password;
-
+    //standard constructor for the User class
     public User(String name, String password) {
         this.name = name;
         this.password = password;
     }
-
+    //returns the name of the user
     public String getName() {
         return name;
     }
-
+    //returns the password of the user
     public String getPassword() {
         return password;
     }
-
+    //returns the information of the user in a formatted string
     public String toString() {
         return String.format("%s, %s", name, password);
     }
