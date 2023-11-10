78eaadcb768593408693ac807f0a07e1caaf98db
steven-chang2023 <chang.steven35@gmail.com>
2023-11-10 02:20:23 -0500

    implemented simple storage tests later will add later

--- Application/test/TestRunner.java
+++ Application/test/TestRunner.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class TestRunner {
+import org.junit.runner.*;
+import org.junit.runner.Result;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(StorageTest.class);
+        System.out.println(result.wasSuccessful());
+    }
 }
